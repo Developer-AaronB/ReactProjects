242d839ff6dc6a7698b46e3ce2d396a27d96ca50
Aaron Botello <aaron.botello.2017@gmail.com>
2024-09-15 18:39:30 -0500

    New Modal Set Up

--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
 import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from  './Modal';
 import classes from './PostList.module.css';
 
 
 function PostList() {
+    const [enteredBody, setEnteredBody ] = useState('');
+    const [enteredAuthor, setEnteredAuthor ] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value)
+    }
     return (
+        <>
+        <Modal>
+        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+        </Modal>
         <ul className={classes.posts}>
-            <Post author="Aaron" body="React.js is awesome" />
-            <Post author="Danny" body="Check out the full course" />
+            <Post author={enteredAuthor} body={enteredBody} />
+            <Post author={enteredAuthor} body={enteredBody} />
         </ul>
+        </>
     );
 }
 
