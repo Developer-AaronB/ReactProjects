f2b780567a6b4af3e743ca0a244b66e896915367
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2024-10-01 20:11:55 -0500

    Fixed Modal

--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -7,8 +7,13 @@ import classes from './PostList.module.css';
 
 
 function PostList() {
+    const [ modalIsVisible, setModalIsVisible ] = useState(true);
     const [enteredBody, setEnteredBody ] = useState('');
     const [enteredAuthor, setEnteredAuthor ] = useState('');
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -18,9 +23,9 @@ function PostList() {
     }
     return (
         <>
-        <Modal>
+        {modalIsVisible ? <Modal onClose={hideModalHandler}>
         <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
-        </Modal>
+        </Modal> : null}
         <ul className={classes.posts}>
             <Post author={enteredAuthor} body={enteredBody} />
             <Post author={enteredAuthor} body={enteredBody} />
